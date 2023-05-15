d93615a363902f829ad39d6438f470981b3f89d0
Jagmit <bitscotch@gmail.com>
2023-05-15 22:10:24 +0530

    Updated Room Service

--- StayWell/src/main/java/com/staywell/model/Room.java
+++ StayWell/src/main/java/com/staywell/model/Room.java
@@ -1,12 +1,20 @@
 package com.staywell.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,15 +25,22 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Room {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@JsonProperty(access = Access.READ_ONLY)
 	private Integer roomId;
+	
 	private Integer roomNumber;
 	private String roomType;
 	private Integer noOfPerson;
 	private BigDecimal price;
 	private Boolean available;
 	
-	@ManyToOne
+	@JsonIgnore
+	@ManyToOne(cascade = CascadeType.ALL)
 	private Hotel hotel;
+	
+	@JsonIgnore
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "room")
+	private List<Reservation> reservations = new ArrayList<>();
 	
 }
