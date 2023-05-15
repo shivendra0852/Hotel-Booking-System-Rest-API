955eb1641bec47841d4b7793fe29d9bfa0e1c92f
pra3eek <prateekchauhan0004@gmail.com>
2023-05-15 20:37:53 +0530

    #3 some updates

--- StayWell/src/main/java/com/staywell/controller/HotelController.java
+++ StayWell/src/main/java/com/staywell/controller/HotelController.java
@@ -3,6 +3,7 @@ package com.staywell.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
