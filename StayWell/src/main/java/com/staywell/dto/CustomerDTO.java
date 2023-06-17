6d5793f40b3b10abf1fe8f39130d5a8ffe9132aa
Jagmit <bitscotch@gmail.com>
2023-06-17 17:41:50 +0530

    Updated Customer model and dto

--- StayWell/src/main/java/com/staywell/dto/CustomerDTO.java
+++ StayWell/src/main/java/com/staywell/dto/CustomerDTO.java
@@ -2,9 +2,17 @@ package com.staywell.dto;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.staywell.enums.Gender;
 import com.staywell.model.Address;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +22,28 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CustomerDTO {
 
+	@NotNull @NotEmpty @NotBlank
     private String name;
 
+	@NotNull @NotEmpty @NotBlank
     private String email;
 
+	@NotNull @NotEmpty @NotBlank
     private String phone;
 
+	@NotNull @NotEmpty @NotBlank
+	@Enumerated(EnumType.STRING)
     private Gender gender;
 
+	@NotNull @NotEmpty @NotBlank
+	@JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate dob;
 
+	@NotNull @NotEmpty @NotBlank
+	@Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}")
     private String password;
 
+	@Valid
     private Address address;
 
 }
--- StayWell/src/main/java/com/staywell/model/Customer.java
+++ StayWell/src/main/java/com/staywell/model/Customer.java
@@ -5,23 +5,28 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.staywell.enums.Gender;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -29,21 +34,24 @@ public class Customer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer customerId;
-	private String name;
 
+	private String name;
 	@Column(unique = true)
 	private String email;
 	private String phone;
 	@Enumerated(EnumType.STRING)
 	private Gender gender;
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private LocalDate dob;
 	private String password;
 	@Embedded
 	private Address address;
+
 	private String role;
-	
-	@JsonIgnore
-	@OneToMany(mappedBy = "customer")
-	private List<Reservation> reservations = new ArrayList<>();
 	private LocalDateTime registrationDateTime;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "customer", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	private List<Reservation> reservations = new ArrayList<>();
+
 }
